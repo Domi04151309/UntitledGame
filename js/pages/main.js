7971d41c583206c0487e060985f9770a8f796e52
Domi04151309 <36272047+Domi04151309@users.noreply.github.com>
2021-07-17 11:32:58 +0200

    Clean up

--- js/pages/main.js
+++ js/pages/main.js
@@ -1,13 +1,8 @@
-import Page from '../components/page.js'
-
 export default {
   name: 'main',
   template:
-  `<page title="Main">
+  `<main>
     <h2>Not Yet Created</h2>
     <p>Unfortunately the site you requested does not exist yet!</p>
-  </page>`,
-  components: {
-    Page
-  }
+  </main>`
 }
--- js/pages/unknown.js
+++ js/pages/unknown.js
@@ -1,14 +1,9 @@
-import Page from '../components/page.js'
-
 export default {
   name: 'unknown',
   template:
-  `<page title="Not Found">
+  `<main>
     <h2>Page Not Found</h2>
     <p class="mb-16">Unfortunately the site you requested does not exist!</p>
     <button type="button" v-on:click="$router.push('/')">Back To Main Menu</button>
-  </page>`,
-  components: {
-    Page
-  }
+  </main>`
 }
